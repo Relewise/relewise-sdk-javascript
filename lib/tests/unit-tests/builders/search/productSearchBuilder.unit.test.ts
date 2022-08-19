b5c62f57b769a7bd93d71c6d97a7f2d8ee0d758a
Martin Zanoni <mza@relewise.com>
2022-08-19 10:16:18 +0200

    expand tests

---
+++ lib/tests/unit-tests/builders/search/productSearchBuilder.unit.test.ts
@@ -0,0 +1,87 @@
+import { UserFactory } from "../../../../src/factory";
+import { ProductSearchBuilder } from "../../../../src/builders/search";
+import { test, expect } from '@jest/globals'
+import { ProductSearchRequest } from "../../../../src/models/data-contracts";
+
+function baseBuilder() {
+    return new ProductSearchBuilder({
+        language: "da-DK",
+        currency: "DKK",
+        displayedAtLocation: "search page",
+        user: UserFactory.anonymous()
+    });
+};
+
+test('setTerm', () => {
+    const expectedTerm = "shoe";
+
+    const subject: ProductSearchRequest = baseBuilder()
+        .setTerm(expectedTerm)
+        .build();
+
+    expect(subject.term).toBe(expectedTerm);
+});
+
+test('reset Term', () => {
+    const expectedTerm = null;
+
+    const subject: ProductSearchRequest = baseBuilder()
+        .setTerm("test")
+        .setTerm(expectedTerm)
+        .build();
+
+    expect(subject.term).toBe(expectedTerm);
+});
+
+test('explodedVariants', () => {
+    const subject: ProductSearchRequest = baseBuilder()
+        .setExplodedVariants(1)
+        .build();
+
+    expect(subject.settings?.explodedVariants).toBe(1);
+});
+
+test('productProperties', () => {
+    const subject: ProductSearchRequest = baseBuilder()
+        .setProductProperties({
+            displayName: true,
+            categoryPaths: true,
+            assortments: true,
+            pricing: true,
+            allData: true,
+            viewedByUserInfo: true,
+            purchasedByUserInfo: true,
+            brand: true,
+            allVariants: true,
+        })
+        .build();
+
+    expect(subject.settings?.selectedProductProperties?.allData).toBe(true);
+});
+
+test('variantProperties', () => {
+    const subject: ProductSearchRequest = baseBuilder()
+        .setVariantProperties({
+            displayName: true,
+            assortments: true,
+            pricing: true,
+            allData: true,
+            allSpecifications: true
+        })
+        .build();
+
+    expect(subject.settings?.selectedVariantProperties?.allSpecifications).toBe(true);
+});
+
+test('brandProperties', () => {
+    const subject: ProductSearchRequest = baseBuilder()
+        .setBrandProperties({
+            displayName: true,
+            assortments: true,
+            allData: true,
+            viewedByUserInfo: true,
+        })
+        .build();
+
+    expect(subject.settings?.selectedBrandProperties?.displayName).toBe(true);
+});

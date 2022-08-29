4683b6dae693155e8ffd9aad74dd18756d440deb
Martin Zanoni <mza@relewise.com>
2022-08-29 10:24:52 +0200

    add datavalue factory

---
+++ lib/src/factory/dataValue.factory.ts
@@ -0,0 +1,39 @@
+import { BooleanCollectionDataValue, BooleanDataValue, DoubleCollectionDataValue, MoneyDataValue, MultiCurrencyDataValue, MultilingualDataValue, NumberDataValue, StringCollectionDataValue, StringDataValue } from '@/models/dataValue';
+
+export class DataValueFactory {
+    static string(value: string): StringDataValue {
+        return new StringDataValue(value);
+    }
+    
+    static stringCollection(collection: string[]): StringCollectionDataValue {
+        return new StringCollectionDataValue(collection);
+    }
+
+    static number(value: number): NumberDataValue {
+        return new NumberDataValue(value);
+    }
+
+    static doubleCollection(collection: number[]): DoubleCollectionDataValue {
+        return new DoubleCollectionDataValue(collection);
+    }
+
+    static boolean(value: boolean): BooleanDataValue {
+        return new BooleanDataValue(value);
+    }
+
+    static booleanCollection(collection: boolean[]): BooleanCollectionDataValue {
+        return new BooleanCollectionDataValue(collection);
+    }
+
+    static money(amount: number, currency: string): MoneyDataValue {
+        return new MoneyDataValue(amount, currency);
+    }
+
+    static multiCurrency(values: { amount: number, currency: string }[]): MultiCurrencyDataValue {
+        return new MultiCurrencyDataValue(values);
+    }
+
+    static multilingual(values: { value: string, language: string }[]): MultilingualDataValue {
+        return new MultilingualDataValue(values);
+    }
+}

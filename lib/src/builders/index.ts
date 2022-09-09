b74eb37a3c49f72b012723ac1ac6f5d8d0cb35d8
Martin Zanoni <mza@relewise.com>
2022-09-09 07:49:58 +0200

    fix missing export

--- lib/src/builders/index.ts
+++ lib/src/builders/index.ts
@@ -2,5 +2,5 @@ export * from './settings';
 export * from './filterBuilder';
 export * from './paginationBuilder';
 export * from './conditionBuilder';
-export * from './relevanceModifiersBuilder';
+export * from './relevanceModifierBuilder';
 export * from './search';

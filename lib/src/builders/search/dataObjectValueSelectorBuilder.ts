5712752569e84c6ae2dc9a0b356c6f6b960e3720
Rasmus Østergaard <100682711+RAORelewise@users.noreply.github.com>
2023-08-30 10:53:57 +0200

    feat: add product data object sorting (#38)
    * feat: add product data object sorting
    
    * rename to builder
    
    * Update dataObjectValueSelectorBuilder.ts

---
+++ lib/src/builders/search/dataObjectValueSelectorBuilder.ts
@@ -0,0 +1,68 @@
+import { DataObjectFilter, DataObjectValueSelector } from 'src/models/data-contracts';
+import { DataObjectFilterConditionBuilder } from '../dataObjectFilterConditionBuilder';
+
+export class DataObjectValueSelectorBuilder {
+    private key!: string;
+    private filter?: DataObjectFilter;
+    private childSelector?: DataObjectValueSelector;
+    private fallbackSelector?: DataObjectValueSelector;
+
+    public select(key: string, settings?: {
+        filter?: {
+            conditions?: (builder: DataObjectFilterConditionBuilder) => void,
+            skip?: number,
+            take?: number 
+        } | null, 
+        childSelector?: (childSelector: DataObjectValueSelectorBuilder) => void | null,
+        fallbackSelector?: (childSelector: DataObjectValueSelectorBuilder) => void | null
+    })
+    {
+        if (!key) 
+            throw new Error('DataObjectValueSelector key can\'t be null or empty')
+
+        this.key = key;
+        
+        if (settings) {
+            if (settings.filter) {
+                const conditionsBuilder = new DataObjectFilterConditionBuilder();
+                if (settings.filter?.conditions) {
+                    settings.filter?.conditions(conditionsBuilder);
+                }
+    
+                this.filter = {
+                    skip: settings.filter.skip,
+                    take: settings.filter.take, 
+                    conditions: conditionsBuilder.build(),
+                }
+            }
+    
+            if (settings.childSelector) {
+                const childSelector = new DataObjectValueSelectorBuilder();
+                if (settings?.childSelector) {
+                    settings?.childSelector(childSelector);
+                }
+                this.childSelector = childSelector.build();
+            }
+
+            if (settings.fallbackSelector) {
+                const fallbackSelector = new DataObjectValueSelectorBuilder();
+                if (settings?.fallbackSelector) {
+                    settings?.fallbackSelector(fallbackSelector);
+                }
+                this.fallbackSelector = fallbackSelector.build();
+            }
+        }
+    }
+
+    public build(): DataObjectValueSelector {
+        if (!this.key) 
+            throw new Error('DataObjectValueSelector key can\'t be null or empty - did you forget to use .select(\'key)?\'')
+    
+        return {
+            key: this.key,
+            filter: this.filter,
+            childSelector: this.childSelector,
+            fallbackSelector: this.fallbackSelector,
+        }
+    }
+}
--- lib/src/builders/search/productSortingBuilder.ts
+++ lib/src/builders/search/productSortingBuilder.ts
@@ -1,10 +1,12 @@
-import { ProductAttributeSorting, ProductDataSorting, ProductPopularitySorting, ProductRelevanceSorting, ProductSortBySpecification, ProductVariantAttributeSorting, ProductVariantSpecificationSorting } from '../../models/data-contracts';
+import { ProductAttributeSorting, ProductDataSorting, ProductDataObjectSorting, ProductPopularitySorting, ProductRelevanceSorting, ProductSortBySpecification, ProductVariantAttributeSorting, ProductVariantSpecificationSorting } from '../../models/data-contracts';
+import { DataObjectValueSelectorBuilder } from './dataObjectValueSelectorBuilder';
 
 export class ProductSortingBuilder {
     private value:
         | ProductAttributeSorting
         | ProductDataSorting
         | ProductPopularitySorting
+        | ProductDataObjectSorting
         | ProductRelevanceSorting
         | ProductVariantAttributeSorting
         | ProductVariantSpecificationSorting
@@ -17,6 +19,22 @@ export class ProductSortingBuilder {
             mode,
             order,
             key,
+            thenBy: this.thenBy(thenBy)?.value,
+        }
+
+        this.value = sort;
+    }
+
+    public sortByProductDataObject(selectionStrategy: 'Product' | 'Variant' | 'VariantWithFallbackToProduct' | 'ProductWithFallbackToVariant', order: 'Ascending' | 'Descending', valueSelector: (valueSelector: DataObjectValueSelectorBuilder) => void, thenBy?: (thenBy: ProductSortingBuilder) => void, mode: 'Auto' | 'Alphabetical' | 'Numerical' = 'Auto') {
+        const valueSelectorFactory = new DataObjectValueSelectorBuilder();
+        valueSelector(valueSelectorFactory);
+        
+        const sort: ProductDataObjectSorting = {
+            $type: 'Relewise.Client.DataTypes.Search.Sorting.Product.ProductDataObjectSorting, Relewise.Client',
+            dataSelectionStrategy: selectionStrategy,
+            mode,
+            order,
+            valueSelector: valueSelectorFactory.build(),
             thenBy: this.thenBy(thenBy)?.value,
         }
 

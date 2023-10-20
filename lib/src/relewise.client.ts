7076956d262ceb117ecf4c0e825c7e31c1ba2fd0
RAORelewise <rao@relewise.com>
2023-10-20 09:10:57 +0200

    Update relewise.client.ts

--- lib/src/relewise.client.ts
+++ lib/src/relewise.client.ts
@@ -50,18 +50,20 @@ export abstract class RelewiseClient {
 
     private readonly _serverUrl: string = 'https://api.relewise.com';
     private readonly _urlPath: string = 'v1';
-    private readonly apiKeyHeader = `APIKey ${this.apiKey}`;
-    private readonly useCancellation: boolean = false;
+    private readonly _apiKeyHeader: string;
+    private readonly _useCancellation: boolean = false;
 
     constructor(protected readonly datasetId: string, protected readonly apiKey: string, options?: RelewiseClientOptions) {
         if (!datasetId) throw new Error('Dataset id cannot be null or empty. Please contact Relewise if you don\'t have an account already or would like a free demo license');
         if (!apiKey) throw new Error('API Key secret cannot be null or empty. Please contact Relewise support if you don\'t know the apiKeySecret for your datasetId.');
 
+        this._apiKeyHeader = `APIKey ${this.apiKey}`;
+
         if (options?.serverUrl) {
             this._serverUrl = options.serverUrl;
         }
 
-        this.useCancellation = options?.useCancellation ?? false;
+        this._useCancellation = options?.useCancellation ?? false;
     }
 
     public get serverUrl(): string {
@@ -74,12 +76,12 @@ export abstract class RelewiseClient {
         const response = await fetch(requestUrl, {
             method: 'POST',
             headers: {
-                Authorization: this.apiKeyHeader,
+                Authorization: this._apiKeyHeader,
                 'Content-Type': 'application/json',
                 'X-Relewise-Version': version.tag,
             },
             body: JSON.stringify(data),
-            ...(this.useCancellation && { signal: this.ensureAbortSignal(requestUrl) }),
+            ...(this._useCancellation && { signal: this.ensureAbortSignal(requestUrl) }),
         });
 
         if (!response.ok) {
@@ -89,7 +91,7 @@ export abstract class RelewiseClient {
             } catch (_) { 
                 console.log(responseMessage)
             } finally {
-                if (this.useCancellation) {
+                if (this._useCancellation) {
                     this.clearAbortSignal(requestUrl);
                 }
             }

da4c2969a56e2f41e64480ddb7ac9bbc3ebc5416
Martin Zanoni <mza@relewise.com>
2023-11-10 12:26:31 +0100

    use dummy values

--- packages/integrations/tests/unit-tests/integrator.unit.test.ts
+++ packages/integrations/tests/unit-tests/integrator.unit.test.ts
@@ -2,9 +2,7 @@
 import { expect, test } from '@jest/globals';
 import { Integrator } from '../../src';
 
-const { npm_config_API_KEY: API_KEY, npm_config_DATASET_ID: DATASET_ID, npm_config_SERVER_URL: SERVER_URL } = process.env;
-
-const integrator = new Integrator(DATASET_ID!, API_KEY!, { serverUrl: SERVER_URL });
+const integrator = new Integrator('dummy value', 'dummy value');
 
 test('Batch size', async() => {
     expect(integrator.batchSize).toBe(1_000);

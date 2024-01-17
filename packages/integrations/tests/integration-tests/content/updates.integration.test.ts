8eecb1e94f242e3bdf253d51ec9bd13bcf3a61f1
SWH-Relewise <swh@relewise.com>
2024-01-17 13:25:35 +0100

    product and content integration examples for docs

--- packages/integrations/tests/integration-tests/content/updates.integration.test.ts
+++ packages/integrations/tests/integration-tests/content/updates.integration.test.ts
@@ -1,6 +1,6 @@
 import { test } from '@jest/globals';
 import { ContentAdministrativeActionBuilder, ContentUpdateBuilder, Integrator } from '../../../src';
-import { DataValueFactory } from '@relewise/client';
+import { DataValueFactory, Trackable } from '@relewise/client';
 const { npm_config_API_KEY: API_KEY, npm_config_DATASET_ID: DATASET_ID, npm_config_SERVER_URL: SERVER_URL } = process.env;
 
 const integrator = new Integrator(DATASET_ID!, API_KEY!, { serverUrl: SERVER_URL });
@@ -59,4 +59,85 @@ test('Create Content', async() => {
         kind: 'Disable',
     });
     await integrator.executeContentAdministrativeAction(disable.build());
+});
+
+
+test('content integration example for docs', async() => {
+    // Copy dataset id + API key + server URL from my.relewise.com > Settings > Developer Settings
+    // const integrator = new Integrator('00000000-0000-0000-0000-000000000001', 'your api key', { serverUrl: 'the server URL for the dataset'});
+
+    // This is the recommended "Push" method, for importing content data into Relewise.
+    
+    // Create a timestamp which will be stored on every content element, this way we can disable all content which has a lower timestamp than this, after the import has completed, effectively removing all content that weren't just updated.
+    // It's important that the timestamp is the exact same for ALL content imported if we want to be able to batch-disable or delete expired content based on it.
+    const date: number = Date.now();
+    const english = 'en';
+
+    const contentUpdates: Trackable[] = [];
+    
+    // Foreach content element that needs to be imported, do the following:
+    {
+        const content = new ContentUpdateBuilder({
+            id: 'Content-ID-01', // The Id should be the primary id of the content which isn't dependent on the title or other editable attributes of the content element.
+            updateKind: 'ReplaceProvidedProperties',
+        })
+            .displayName([
+                { language: english, value: 'The english display name' }, // We only set the english translation in this example but more can be set by parsing more Multilingual.Value.
+            ])
+            .data({
+                'ShortDescription': DataValueFactory.multilingual([{language: english, value: 'The short english description'}]), // We only set the english translation in this example but more can be set by parsing more Multilingual.Value. 
+                'ImportedAt': DataValueFactory.number(date), // Important to set this timestamp to the exact same value for all content being updated as part of this full (not-delta) update, as we will use this timestamp to remove content that was not just imported, assuming they must have been removed since the last import and therefore should also be disabled in Relewise.
+                'ReadingTimeInMinutes': DataValueFactory.number(3),
+                'News': DataValueFactory.boolean(true),
+                'Badges': DataValueFactory.stringCollection(['fun', 'current season', 'some other badge']),
+            // Add any additional fields you would want returned from Relewise as part of search results
+            })
+            .categoryPaths(b => b
+                .path(p => p
+                    .category({
+                        id: '23',
+                        displayName: [{ language: english, value: 'Outdoor' }],
+                    })
+                    .category({
+                        id: '372',
+                        displayName: [{ language: english, value: 'Hiking' }],
+                    }),
+                ));
+
+        contentUpdates.push(content.build());
+    }
+    // Foreach content element END
+
+    // If this is a full-import (not delta) disable all non-included content elements
+    {
+        const disableNonUpdatedContet = new ContentAdministrativeActionBuilder({
+            language: null,
+            currency: null,
+            filters(filterBuilder) {
+                filterBuilder
+                    .addContentDataFilter(
+                        'ImportedAt',
+                        (conditionBuilder) => conditionBuilder.addEqualsCondition(DataValueFactory.number(date), true),
+                    );
+            },
+            kind: 'Disable',
+        });
+        contentUpdates.push(disableNonUpdatedContet.build());
+    }
+
+    const enableUpdatedContent = new ContentAdministrativeActionBuilder({
+        language: null,
+        currency: null,
+        filters(filterBuilder) {
+            filterBuilder
+                .addContentDataFilter(
+                    'ImportedAt',
+                    (conditionBuilder) => conditionBuilder.addEqualsCondition(DataValueFactory.number(date), false),
+                );
+        },
+        kind: 'Enable',
+    });
+    contentUpdates.push(enableUpdatedContent.build());
+
+    await integrator.batch(contentUpdates);
 });
--- packages/integrations/tests/integration-tests/products/updates.integration.test.ts
+++ packages/integrations/tests/integration-tests/products/updates.integration.test.ts
@@ -1,6 +1,6 @@
-import { test, expect } from '@jest/globals';
+import { test } from '@jest/globals';
+import { DataValueFactory, ProductVariant, Trackable } from '@relewise/client';
 import { Integrator, ProductAdministrativeActionBuilder, ProductUpdateBuilder, ProductVariantBuilder } from '../../../src';
-import { DataValueFactory } from '@relewise/client';
 const { npm_config_API_KEY: API_KEY, npm_config_DATASET_ID: DATASET_ID, npm_config_SERVER_URL: SERVER_URL } = process.env;
 
 const integrator = new Integrator(DATASET_ID!, API_KEY!, { serverUrl: SERVER_URL });
@@ -150,4 +150,117 @@ test('Create Product with variants', async() => {
         .variants([variant1, variant2]);
 
     await integrator.updateProduct(product.build());
+});
+
+test('product integration example for docs', async() => {
+    // Copy dataset id + API key + server URL from my.relewise.com > Settings > Developer Settings
+    // const integrator = new Integrator('00000000-0000-0000-0000-000000000001', 'your api key', { serverUrl: 'the server URL for the dataset'});
+
+    // This is the recommended "Push" method, for importing product data into Relewise.
+
+    // Create a timestamp which will be stored on every product, this way we can disable all products which have a lower timestamp than this, after the import has completed, effectively removing all products that weren't just updated.
+    // It's important that the timestamp is the exact same for ALL products imported if we want to be able to batch-disable or delete expired products based on it.    
+    const date: number = Date.now();
+
+    const english = 'en';
+    const dkk = 'dkk';
+    
+    const productUpdates: Trackable[] = [];
+        
+    // Foreach product that needs to be imported, do the following:
+    {
+        const variants: ProductVariant[] = [];
+        // Foreach variant of this product
+        {
+            const variant: ProductVariant = {
+                id: 'The variant id', // Variant id only has to be unique within the product
+                data: {
+                    'Materials': DataValueFactory.multilingualCollection([{ values: ['Wood', 'Metal'], language: english }]),
+                    'Colors': DataValueFactory.multilingualCollection([{ values: ['Red', 'Green'], language: english }]),
+                    'PrimaryMaterial': DataValueFactory.multilingual([{value: 'Wood', language: english}]),
+                    'PrimaryColor': DataValueFactory.multilingual([{value: 'Red', language: english}]),
+                },
+            };
+    
+            variants.push(variant);
+        }
+        // Foreach variant END
+    
+        const product = new ProductUpdateBuilder({
+            id: 'Product-SKU-01', // The Id should be the primary id of the product, which can be used to identify the product even outside the website for best compatibility with future integrations, like ERP order imports from other sales channels etc.
+            productUpdateKind: 'ReplaceProvidedProperties',
+            variantUpdateKind: 'ReplaceProvidedProperties',
+            replaceExistingVariants: true, // Replace existing variants = true means that any other variants on the product, currenty existing in Relewise but not included in this update request, will be deleted, for full updates this is desired however for delta updates, this should obviously be set to false to avoid implicitly deleting non-modified variants.
+        })
+            .displayName([
+                { language: english, value: 'The english display name' }, // We only set the english translation in this example but more can be set by parsing more Multilingual.Value.
+            ])
+            .listPrice([{currency: dkk, amount: 199}])
+            .brand({
+                id: 'brandId',
+                displayName: 'Brand display name', // If you dont have both id and displayname for brands, displayname can be left out, id is required
+            })
+            .data({
+                'ShortDescription': DataValueFactory.multilingual([{language: english, value: 'The short english description'}]), // We only set the english translation in this example but more can be set by parsing more Multilingual.Value. 
+                'ImportedAt': DataValueFactory.number(date), // Important to set this timestamp to the exact same value for all content being updated as part of this full (not-delta) update, as we will use this timestamp to remove content that was not just imported, assuming they must have been removed since the last import and therefore should also be disabled in Relewise.
+                'MinimumAge': DataValueFactory.number(4),
+                'InStock': DataValueFactory.boolean(true),
+                'USPs': DataValueFactory.stringCollection(['first usp', 'second usp', 'third usp']),
+                // Add any additional fields you would want returned from Relewise as part of search results
+            })
+            .categoryPaths(b => b
+                .path(p => p
+                    .category({
+                        id: '74',
+                        displayName: [{ language: english, value: 'Play' }],
+                    })
+                    .category({
+                        id: '2',
+                        displayName: [{ language: english, value: 'Swings' }],
+                    })
+                    .category({
+                        id: '529',
+                        displayName: [{ language: english, value: 'Swing Seats' }],
+                    }),
+                ))
+            .variants(variants);
+    
+        productUpdates.push(product.build());
+    }
+    // Foreach content element END
+    
+    // If this is a full-import (not delta) disable all non-included content elements
+    {
+        const disableNonUpdatedContet = new ProductAdministrativeActionBuilder({
+            language: null,
+            currency: null,
+            filters(filterBuilder) {
+                filterBuilder
+                    .addContentDataFilter(
+                        'ImportedAt',
+                        (conditionBuilder) => conditionBuilder.addEqualsCondition(DataValueFactory.number(date), true),
+                    );
+            },
+            productUpdateKind: 'Disable',
+        });
+        productUpdates.push(disableNonUpdatedContet.build());
+    }
+    
+    const enableUpdatedContent = new ProductAdministrativeActionBuilder({
+        language: null,
+        currency: null,
+        filters(filterBuilder) {
+            filterBuilder
+                .addContentDataFilter(
+                    'ImportedAt',
+                    (conditionBuilder) => conditionBuilder.addEqualsCondition(DataValueFactory.number(date), false),
+                );
+        },
+        productUpdateKind: 'Enable',
+    });
+    
+    productUpdates.push(enableUpdatedContent.build());
+    
+    // Send all product updates and the enable/disable action as a single (or few) network requests, the client will automatically send batches of 1.000 items per network request so you can pass as many as you like to the tracker.Track / TrackAsync method.
+    await integrator.batch(productUpdates);
 });

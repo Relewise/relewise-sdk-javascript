1f73c820f62ce765facf1b4ab445a2ea6b336908
RAORelewise <rao@relewise.com>
2024-06-27 12:08:12 +0200

    fix backwards compatibility

--- packages/client/src/builders/filterBuilder.ts
+++ packages/client/src/builders/filterBuilder.ts
@@ -8,6 +8,11 @@ import { ProductFilterBuilder } from './filters/productFilterBuilder';
 import { VariantFilterBuilder } from './filters/variantFilterBuilder';
 import { AllFilters, FilterOptions, EntityDataFilterOptions } from './filters/filters.types.shared';
 
+export { // Needed for backwards compatibility
+    FilterOptions,
+    EntityDataFilterOptions,
+};
+
 export class FilterBuilder {
     private filters: AllFilters[] = [];
 

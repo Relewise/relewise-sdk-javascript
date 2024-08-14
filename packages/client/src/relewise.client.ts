3437337fa6a735033b4963ffbff3d3f36f2f4a40
RAORelewise <rao@relewise.com>
2024-08-14 13:51:12 +0200

    feat: print errors to console

--- packages/client/src/relewise.client.ts
+++ packages/client/src/relewise.client.ts
@@ -72,14 +72,16 @@ export abstract class RelewiseClient {
             } catch (_) { 
             }
 
+            console.error(responseMessage);
+
             throw new ProblemDetailsError('Error when calling the Relewise API. Read more in the details property if there is error response or look in the network tab.', responseMessage);
         }
 
         try {
             const responseMessage = await response.json();
-
             return responseMessage as TResponse;
         } catch (err) {
+            console.error(err);
             return undefined;
         }
     }

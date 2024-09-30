5f48e07238fc2868a2531e41c0978354e426937b
Martin Zanoni <mza@relewise.com>
2024-09-30 13:03:50 +0200

    fix

--- packages/integrations/rollup.config.mjs
+++ packages/integrations/rollup.config.mjs
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
-import pkg from './package.json';
+import pkg from './package.json' with { type: "json" };;
 
 export default [
     // CommonJS (for Node) and ES module (for bundlers) build.

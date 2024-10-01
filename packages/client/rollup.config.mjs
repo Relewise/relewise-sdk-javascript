f469699c238ff62e00b056e95d1b770abe374ac2
Martin Zanoni <mza@relewise.com>
2024-10-01 10:09:41 +0200

    fix

--- packages/client/rollup.config.mjs
+++ packages/client/rollup.config.mjs
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import json from '@rollup/plugin-json';
-import pkg from './package.json' with { type: "json" };;
+import pkg from './package.json' with { type: "json" };
 import terser from '@rollup/plugin-terser';
 
 export default [

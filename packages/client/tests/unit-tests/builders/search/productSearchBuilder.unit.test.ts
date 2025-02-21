a25f5c67c360fa3ea0a51b52ad105d9a90965216
Martin Zanoni <mza@relewise.com>
2025-02-21 09:43:16 +0100

    make test build

--- packages/client/tests/unit-tests/builders/search/productSearchBuilder.unit.test.ts
+++ packages/client/tests/unit-tests/builders/search/productSearchBuilder.unit.test.ts
@@ -122,6 +122,7 @@ test('searchHightlighting', () => {
             h.setShape({
                 includeOffsets: true,
                 textSnippets: {
+                    includeEllipses: true,
                     includeTextSnippets: true,
                 }
             });

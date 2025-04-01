978f760176b16bdd8ded1173fb62741008a4e7bf
Martin Zanoni <mza@relewise.com>
2025-04-01 08:53:18 +0200

    tweaks

--- packages/client/src/relewise.client.ts
+++ packages/client/src/relewise.client.ts
@@ -51,13 +51,9 @@ export abstract class RelewiseClient {
         return this._serverUrl;
     }
 
-    protected async request<TRequest, TResponse>(
-        name: string,
-        data: TRequest,
-        options?: RelewiseRequestOptions
-    ): Promise<TResponse | undefined> {
+    protected async request<TRequest, TResponse>(name: string, data: TRequest, options?: RelewiseRequestOptions): Promise<TResponse | undefined> {
         const requestUrl = this.createRequestUrl(this._serverUrl, this.datasetId, this._urlPath, name);
-    
+
         try {
             const response = await fetch(requestUrl, {
                 method: 'POST',
@@ -70,44 +66,25 @@ export abstract class RelewiseClient {
                 signal: options?.abortSignal,
                 cache: 'no-cache',
             });
+
+            if (!response.ok) {
+                let responseMessage: HttpProblemDetails | null = null;
     
-            this.ensureResponseOk(response);
+                try { 
+                    responseMessage = await response.json();
+                } catch (_) { 
+                }
     
+                const details = responseMessage?.detail ? `Details: ${responseMessage.detail}\n` : '';
+    
+                throw new ProblemDetailsError(`Error when calling the Relewise API.\n\nTitle: ${response.statusText}\nStatus: ${response.status}\n${details}\nRead more in the details property if there is error response or look in the network tab.`, responseMessage);
+            }
+
             return await this.parseJson<TResponse>(response);
+
         } catch (err) {
-            this.handleRequestError(err);
+           this.handleRequestError(err);
         }
-    }
-    
-    private async ensureResponseOk(response: Response) {
-        if (response.ok) return;
-    
-        let problemDetails: HttpProblemDetails | null = null;
-        
-        try {
-            problemDetails = await response.json();
-        } catch (_) { /* Ignore JSON parsing errors */ }
-    
-        const details = problemDetails?.detail ? `Details: ${problemDetails.detail}\n` : '';
-    
-        throw new ProblemDetailsError(
-            `Error when calling the Relewise API.\n\nTitle: ${response.statusText}\nStatus: ${response.status}\n${details}\nRead more in the details property if there is error response or look in the network tab.`, 
-            problemDetails);
-    }
-    
-    private async parseJson<T>(response: Response): Promise<T | undefined> {
-        try {
-            return await response.json();
-        } catch {
-            return undefined;
-        }
-    }
-    
-    private handleRequestError(err: unknown): never {
-        if (err instanceof ProblemDetailsError) throw err;
-    
-        console.error("Network error or preflight request failed. Check API Key and Dataset Id.");
-        throw new Error("Network error or preflight request failed. Check API Key and Dataset Id.");
     }
 
     private createRequestUrl(baseUrl: string, ...segments: string[]) {
@@ -115,5 +92,20 @@ export abstract class RelewiseClient {
         return baseUrl.endsWith('/')
             ? baseUrl.concat(joinedSegments)
             : baseUrl.concat('/', joinedSegments);
+    }
+
+    private handleRequestError(err: unknown): never {
+        if (err instanceof ProblemDetailsError) throw err;
+    
+        console.error("Network error or preflight request failed. Check API Key and Dataset ID.");
+        throw new Error("Network error or preflight request failed. Check API Key and Dataset ID.");
+    }
+
+    private async parseJson<T>(response: Response): Promise<T | undefined> {
+        try {
+            return await response.json();
+        } catch {
+            return undefined;
+        }
     }
 }

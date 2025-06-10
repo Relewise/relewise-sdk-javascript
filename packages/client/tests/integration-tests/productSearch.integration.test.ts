55ab8ea56b75578c7fa849626bfae1f96f2a36d8
Martin Zanoni <mza@relewise.com>
2025-06-10 10:23:32 +0200

    test

--- packages/client/tests/integration-tests/productSearch.integration.test.ts
+++ packages/client/tests/integration-tests/productSearch.integration.test.ts
@@ -215,7 +215,7 @@ test('ProductSearch with sorted facet', async () => {
 
     const request: ProductSearchRequest = baseProductBuilder()
         .facets(f =>
-            f.addCategoryFacet("ImmediateParent", null, b => b.take(1).sortByHits())
+            f.addCategoryFacet("ImmediateParent", null, b => b.sortByHits())
         )
         .build();
 
